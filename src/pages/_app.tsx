548502dbb7c960ee925652575e30900447bc0f07
LuisSouza01 <luis.f.souza@outlook.com>
2024-04-23 08:13:04 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Image from 'next/image';
+
+import logoImg from '@/../public/assets/logo.svg';
+
+import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image 
+          src={logoImg}
+          alt="Ignite Shop" 
+          width={129.74} 
+          height={52} 
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
